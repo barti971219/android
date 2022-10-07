4cad03d3bf3e5524ddd7aeb6b6bf75d8c98bd00d
pandabaeung <69721328+barti971219@users.noreply.github.com>
2022-10-07 15:33:49 +0900

    ViewControl

---
+++ MyApplication/app/src/main/java/com/hee/myapplication/TestActivity.kt
@@ -0,0 +1,14 @@
+package com.hee.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+
+    }
+}
